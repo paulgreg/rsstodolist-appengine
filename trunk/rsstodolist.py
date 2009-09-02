be2b30ecdf64f8a6dbdc16f5e510cd9651f10b6d
paulgreg <paulgreg@d9e5b954-9409-11de-9a8c-5f207592b8ba>
2009-09-02 20:03:30 +0000

    Use name instead of username (it's shorter) + randomize front page url

--- trunk/rsstodolist.py
+++ trunk/rsstodolist.py
@@ -13,16 +13,16 @@ from google.appengine.ext.webapp import template
 
 
 class Feed(db.Model):
-  username = db.StringProperty(multiline=False)
+  name = db.StringProperty(multiline=False)
   url = db.StringProperty(multiline=False)
   title = db.StringProperty(multiline=False)
   creation_date = db.DateTimeProperty(auto_now_add=True)  
 
 
-def addUrl(url, username, title):
+def addUrl(url, name, title):
   feed = Feed()
   feed.url = url
-  feed.username = username
+  feed.name = name
 
   if title:
     feed.title = title
@@ -39,38 +39,46 @@ def addUrl(url, username, title):
 
   feed.put()
 
-def renderRss(self, username):
-  feeds = db.GqlQuery('SELECT * FROM Feed WHERE username = :1 ORDER BY creation_date DESC LIMIT 50', username)
+def renderRss(self, name):
+  feeds = db.GqlQuery('SELECT * FROM Feed WHERE name = :1 ORDER BY creation_date DESC LIMIT 50', name)
 
   self.response.headers['Content-Type'] = 'application/rss+xml'
   path = os.path.join(os.path.dirname(__file__), 'rss.xml')
-  self.response.out.write(template.render(path,	{ 'username': username, 'feeds': feeds } ))
+  self.response.out.write(template.render(path,	{ 'name': name, 'feeds': feeds } ))
 
 def goToHome(self):
+  random_url = 'http://www.google.com'
+  result = urlfetch.fetch('http://stackoverflow.com/feeds', allow_truncated=True)
+  if result.status_code == 200:
+    m = re.search('(?<=<id>)http://stackoverflow.com/questions/[0-9]*/', result.content)
+    if m.group(0):
+      random_url = m.group(0)
+
   self.response.headers['Content-Type'] = 'text/html'
-  self.response.out.write(template.render(os.path.join(os.path.dirname(__file__), 'home.html'), { } ))
+  path = os.path.join(os.path.dirname(__file__), 'home.html')
+  self.response.out.write(template.render(path, { 'random_url': random_url } ))
 
 class MainPage(webapp.RequestHandler):
   def get(self): 
-    username = self.request.get('username')
-    if not username:
+    name = self.request.get('name')
+    if not name:
       goToHome(self)
     else:
-      renderRss(self, username)
+      renderRss(self, name)
 
 class AddPage(webapp.RequestHandler):
   def get(self): 
-    username = self.request.get('username')
-    if not username:
+    name = self.request.get('name')
+    if not name:
       goToHome(self)
     else:
       url = self.request.get('url')
       title  = self.request.get('title')
 
       if url:
-        addUrl(url, username, title)
+        addUrl(url, name, title)
 
-      self.redirect('/?username=' + username)
+      self.redirect('/?name=' + name)
 
 
 
