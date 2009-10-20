b0c3887b4f23207d05df4cfe5b635b1bae0e21b3
paulgreg <paulgreg@d9e5b954-9409-11de-9a8c-5f207592b8ba>
2009-10-20 07:27:52 +0000

    Save last feed name

--- firefox-jetpack-extension/rsstodolist-jetpack.js
+++ firefox-jetpack-extension/rsstodolist-jetpack.js
@@ -1,16 +1,23 @@
+jetpack.future.import("storage.simple");
+
 jetpack.statusBar.append({
   html: <>
     <style><![CDATA[
-      body,input {font-family: sans-serif; font-size: 9pt;}
-      input#feed { width: 75px; }
-      input#submit { cursor: pointer; }
+      body,input {font-family: sans-serif; font-size: 9pt; background-color: transparent; color: black;}
+      input#submit { cursor: pointer; width: 50px; background-color: #E3E9EF; }
+      input#feed { width: 75px; background-color: #E3E9EF;}
       input { border: 1px solid black; }
-      a { border-bottom: 1px dotted blue; cursor: pointer; }
+      a { border-bottom: 1px solid #63ADF5; cursor: pointer; }
     ]]></style>
     <body><input type="submit" id="submit" value="add url"/> to <a id="link">rsstodolist/?name=</a><input type="text" id="feed" value="somename"/></body>
   </>,
   width: 280,
   onReady: function(widget){
+
+     var datastore = jetpack.storage.simple;
+     
+     if (datastore.feed && datastore.feed.name != '')
+        $("#feed", widget).val(datastore.feed.name); // Restoring last feed name
 
     $("#link", widget).click(function(){
          var tab = jetpack.tabs.open('http://rsstodolist.appspot.com/?name=' + $("#feed", widget).val());
@@ -20,12 +27,17 @@ jetpack.statusBar.append({
     $("#submit", widget).click(function(){
       var feedName = $("#feed", widget).val();
       if (feedName != '') {
+
+        datastore.feed = { name: feedName }; // Saving feed name
+
         var url = jetpack.tabs.focused.url;
+        
         var request = new XMLHttpRequest();
         request.open("GET", "http://rsstodolist.appspot.com/add?name=" + feedName + "&url=" + url, true);
         request.onreadystatechange = function() {
                 var done = 4, ok = 200;
                 if (request.readyState == done) {
+                    $("#submit", widget).val('add url');
                     $("#submit", widget).attr('disabled', '');
                     if (request.status == ok)
                         jetpack.notifications.show(
@@ -37,6 +49,7 @@ jetpack.statusBar.append({
                                     body: 'Problem while adding URL [' + url + '] to feed '+feedName} );
                }
         };
+        $("#submit", widget).val('adding...');
         $("#submit", widget).attr('disabled', 'disabled'); 
         request.send(null);
       };
