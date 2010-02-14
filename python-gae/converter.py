74db21b738f87e67d2ed7575426c15a882148434
paulgreg <paulgreg@d9e5b954-9409-11de-9a8c-5f207592b8ba>
2010-02-14 16:13:50 +0000

    Fix accent

--- python-gae/converter.py
+++ python-gae/converter.py
@@ -7,8 +7,8 @@ class Converter():
   def convert(self,nameToClean):
     sanitizedName = BeautifulStoneSoup( nameToClean, convertEntities=BeautifulStoneSoup.HTML_ENTITIES )
     unicodedName = unicode( sanitizedName )
-    cleanedName = unicodedName.replace('»'.decode('utf8'), '-')
-    cleanedName = cleanedName.replace('&eacute;', 'é'.decode('utf8'))
+    cleanedName = unicodedName.replace('»'.decode('utf8'), '\'')
+    cleanedName = cleanedName.replace('«'.decode('utf8'), '\'')
     cleanedName = cleanedName.replace('&', 'and')
     cleanedName = cleanedName.replace('<', '{').replace('>', '}')
     cleanedName = cleanedName.replace('\n', ' ').replace('\r', '')
