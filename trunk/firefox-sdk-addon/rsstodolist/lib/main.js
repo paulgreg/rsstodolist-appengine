73e6e58bbd8867f8b0c8835b887709f58d90c29f
a.thieriot@gmail.com <a.thieriot@gmail.com@d9e5b954-9409-11de-9a8c-5f207592b8ba>
2011-04-15 17:53:30 +0000

    addon ready for sdk beta 4 and stop logging anything in info

--- trunk/firefox-sdk-addon/rsstodolist/lib/main.js
+++ trunk/firefox-sdk-addon/rsstodolist/lib/main.js
@@ -23,6 +23,7 @@ var panel = panels.Panel({
 });
 
 var widget = widgets.Widget({
+   id: "rsstodolist",
    label: "RSS Todo List",
    contentURL: data.url("icon.png"),
    panel: panel,
@@ -35,23 +36,23 @@ function transmitInfos() {
    var title = tabs.activeTab.title;
    var feed = simpleStorage.storage.feed;
 
-   console.log('ready - ' + ' description : ' + title + ' feed : ' + feed);
+   console.debug('ready - ' + ' description : ' + title + ' feed : ' + feed);
 
    this.panel.postMessage({description: title, feed: feed});
 }
 
 function doAction(message) {
-   console.log("message - action : " + message.action + " description : " + message.description + " feed : " + message.feed);
+   console.debug("message - action : " + message.action + " description : " + message.description + " feed : " + message.feed);
    if(message.feed && message.action) {
       simpleStorage.storage.feed = message.feed;
 
       switch(message.action) {
          case "link":
-             console.log(serverUrl + "?name=" + message.feed);
+             console.debug(serverUrl + "?name=" + message.feed);
              tabs.open(serverUrl + "?name=" + message.feed); break;
          case "add": requestAddOrDel(message); break;
          case "del": requestAddOrDel(message); break;
-         default: console.log(message.action);
+         default: console.debug(message.action);
       }
    }
    panel.hide();
@@ -59,18 +60,17 @@ function doAction(message) {
 
 function requestAddOrDel(message) {
    var url = constructAddOrDelUrl(message);
-   console.log(url);
+   console.debug(url);
    Request({
       url: url,
       onComplete: function(response){
          if(response.status == 200) {
-            console.log("Sending request OK : " + response.status);
             notifications.notify({
                text: "Le flux " + simpleStorage.storage.feed + " a correctement été mis à jour",
                iconURL: data.url("icon.png")
             });
          } else {
-            console.log("Sending request ERROR : " + response.status);
+            console.error("Sending request ERROR : " + response.status);
             notifications.notify({
                text: "Un problème s'est produit lors de la mise à jour du flux  " + simpleStorage.storage.feed,
                iconURL: data.url("icon.png")
@@ -82,5 +82,3 @@ function requestAddOrDel(message) {
 function constructAddOrDelUrl(message) {
    return serverUrl + message.action + "?name=" + message.feed + "&description=" + encodeURIComponent(message.description || "") + "&url=" + encodeURIComponent(tabs.activeTab.url);
 }
-
-console.log("The add-on is running.");
